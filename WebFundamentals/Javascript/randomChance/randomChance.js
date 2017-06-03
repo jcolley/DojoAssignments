2b8ab8aed91277cba02f7d1e059dac7ddcdba882
John Colley <webjocky@gmail.com>
2017-06-03 10:30:23 -0700

    added randomChance (slot machine) project

--- WebFundamentals/Javascript/randomChance/randomChance.js
+++ WebFundamentals/Javascript/randomChance/randomChance.js
@@ -1,11 +1,28 @@
-function randomChance(num,leave){
-    var rand = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    var prize = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
-    var quarters = num;
+function randomChance(quarters,leave){
+  quarters -= 1;
+  var prize = 0;
+  var rand1 = genRandom();
+  var rand2 = genRandom();
 
-    
-  return quarters; 
+  console.log(rand1 +" | "+rand2)
+
+  if(rand1 === rand2){
+    prize = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
+  }
+  quarters += prize;
+  return quarters;
 }
 
+function genRandom(){
+  return Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+}
 
-randomChance(10,20);
+var quarters = 10;
+var leave = 200;
+
+while(quarters<leave && quarters){
+  quarters = randomChance(quarters,leave);
+  console.log(quarters);
+}
+
+if(quarters>0){console.log("YOU WIN!");}else{console.log("Lost it all...");}
