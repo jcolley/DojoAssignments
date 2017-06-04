7478df31ed32b77560e8f8d313cc4c5506131991
John Colley <webjocky@gmail.com>
2017-06-03 23:31:16 -0700

    added studentsAndInstructors project

---
+++ WebFundamentals/Javascript/studentsAndInstructors/mian.js
@@ -0,0 +1,34 @@
+var students = [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
+
+for(var i=0;i<students.length;i++){
+    var student = students[i];
+    console.log(student.first_name,student.last_name);
+}
+
+var users = {
+ 'Students': [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+  ],
+ 'Instructors': [
+     {first_name : 'Michael', last_name : 'Choi'},
+     {first_name : 'Martin', last_name : 'Puryear'}
+  ]
+ }
+console.log("------------------");
+console.log("Students");
+ for(var i=0;i<users.Students.length;i++){
+    console.log(i+ ' - ' +users.Students[i].first_name,users.Students[i].last_name+ ' - ' +users.Students[i].first_name.length+users.Students[i].last_name.length);
+ }
+
+ console.log("Instructors");
+ for(var i=0;i<users.Instructors.length;i++){
+    console.log(i+' - '+users.Instructors[i].first_name,users.Instructors[i].last_name+ ' - ' +users.Instructors[i].first_name.length+users.Instructors[i].last_name.length);
+ }
