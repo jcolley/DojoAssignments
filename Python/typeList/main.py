f8ea0af50124b9cf772ab7bb7d594e2e012f299c
John Colley <webjocky@gmail.com>
2017-06-06 15:41:04 -0700

    updated typeList assignment

--- Python/typeList/main.py
+++ Python/typeList/main.py
@@ -1,35 +1,35 @@
 def typeList(thing):
-    a = 0
-    b = ""
-    c = "mixed"
+    integer_sum = 0
+    output_string = ""
+    output_type = "mixed"
     output = ""
     for i in thing:
-        if(isinstance(i,int) or isinstance(i,float)):
-            a += i
-        elif(isinstance(i,str)):
-            b += i
+        if isinstance(i, int) or isinstance(i, float):
+            integer_sum += i
+        elif isinstance(i, str):
+            output_string += i
 
-    if(a == 0):
-        if(b == ""):
-            c = "No values"
+    if integer_sum == 0:
+        if output_string == "":
+            output_type = "No values"
         else:
-            c = "string"
+            output_type = "string"
     else:
-        if(b == ""):
-            c = "integer"
-    A = "Sum: "+str(a)
-    B = "String: "+b
-    C = "The array you entered is of "+c+" type."
-    
+        if output_string == "":
+            output_type = "integer"
+    A = "Sum: "+str(integer_sum)
+    B = "String: "+output_string
+    C = "The array you entered is of "+output_type+" type."
+
     output += C + "\n"
 
-    if(b):
+    if output_string:
         output += B + "\n"
-    if(a):
+    if integer_sum:
         output += A + "\n"
 
     return output
 
-l = ['magical','unicorns']
+l = ['magical unicorns',19,'hello',98.98,'world']
 
 print typeList(l)
