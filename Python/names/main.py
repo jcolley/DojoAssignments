10f4b40bd22ec4c66e47677bd32502884ac06629
John Colley <webjocky@gmail.com>
2017-06-06 13:01:53 -0700

    added names assignment

---
+++ Python/names/main.py
@@ -0,0 +1,29 @@
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+for i in students:
+    print i['first_name'], i['last_name']
+print "*" * 40
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key,val in users.iteritems():
+    count = 1
+    print key
+    for i in val:
+        print str(count),"-",i['first_name'],i['last_name'],"-",str(len(i['first_name']+i['last_name']))
