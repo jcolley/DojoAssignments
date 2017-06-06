b634ed08e4dfc9e0ddafdea3f18601c10651b685
John Colley <webjocky@gmail.com>
2017-06-06 12:24:29 -0700

    added stars assignment

---
+++ Python/stars/main.py
@@ -0,0 +1,10 @@
+def stars(arr):
+    for i in arr:
+        if isinstance(i, str):
+            s = i[0]
+            print i[0].lower() * len(i) + "\n"
+        else:
+            print "*" * i + "\n"
+
+x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
+stars(x)
