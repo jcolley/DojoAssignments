bbdbc34a0b49447104465256be8ca01f3016400c
John Colley <webjocky@gmail.com>
2017-06-09 10:35:46 -0700

    added various non-assignment steps and the landing_page assignment

---
+++ Python/virtualenv/flaskEnv/landing_page/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template('ninjas.html')
+
+
+@app.route('/dojos/new')
+def new():
+    return render_template('dojos.html', action='')
+
+app.run(debug=True)  # run our server
---
+++ Python/virtualenv/flaskEnv/template_rendering/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+# our index route will handle rendering our form
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", name="Jay")
+# this route will handle our form submission
+# notice how we defined which HTTP methods are allowed by this route
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    name = request.form['name']
+    email = request.form['email']
+    # Here's the line that changed. We're rendering a template from a post route now.
+    return render_template('success.html')
+
+
+app.run(debug=True)  # run our server
