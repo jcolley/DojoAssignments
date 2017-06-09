d25c46dd804929bde70a748d73626e652db91991
John Colley <webjocky@gmail.com>
2017-06-09 11:30:52 -0700

    added dojoSurvey assignment

---
+++ Python/virtualenv/flaskEnv/dojoSurvey/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/result', methods=['POST'])
+def create_user():
+    print request.form
+
+    return render_template(
+        'result.html',
+        name=request.form['name'],
+        location=request.form['location'],
+        language=request.form['language'],
+        comment=request.form['comment']
+    )
+
+
+app.run(debug=True)
