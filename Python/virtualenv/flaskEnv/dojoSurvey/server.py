4406c99ff6fc424980701b16932b2aa56683c88d
John Colley <webjocky@gmail.com>
2017-06-11 22:41:06 -0700

    updated dojoSurvey with form validation

--- Python/virtualenv/flaskEnv/dojoSurvey/server.py
+++ Python/virtualenv/flaskEnv/dojoSurvey/server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session
 app = Flask(__name__)
+
+app.secret_key = "sadfhijfiu*&#3#79#ih#(78!"
 
 
 @app.route('/')
@@ -9,14 +11,26 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def create_user():
-    print request.form
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
 
+    if len(name) < 1:
+        flash("Name cannot be blank!")
+        return redirect('/')
+    elif len(comment) < 1:
+        flash("You must provide a comment!")
+        return redirect('/')
+    elif len(comment) > 120:
+        flash("You talk too much!")
+        return redirect('/')
     return render_template(
         'result.html',
-        name=request.form['name'],
-        location=request.form['location'],
-        language=request.form['language'],
-        comment=request.form['comment']
+        name=name,
+        location=location,
+        language=language,
+        comment=comment
     )
 
 
