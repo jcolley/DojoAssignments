48e3bbc338e4c1a2beb162adbfe62b47dfde32be
John Colley <webjocky@gmail.com>
2017-06-13 13:25:00 -0700

    added friendships assignment

---
+++ Python/MySQL/friendships/friendships.sql
@@ -0,0 +1,1 @@
+SELECT users.first_name, users.last_name, user2.first_name AS friend_first_name, user2.last_name AS friend_last_name FROM users LEFT JOIN friendships ON users.id = friend_id LEFT JOIN users AS user2 ON user2.id = friend_id ORDER BY user2.last_name;
