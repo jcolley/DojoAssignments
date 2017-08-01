4044d6c8dda009171c030ac0ead9cd79485861d9
John Colley <webjocky@gmail.com>
2017-07-31 23:41:17 -0700

    added first_csharp assignment

---
+++ csharp/first_csharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace first_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for (int i = 1; i <= 255; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // for (int i = 1; i <= 100; i++)
+            // {
+            //     if(i%5 == 0 || i%3 == 0) {
+            //      Console.WriteLine(i);
+            //     }
+            // }
+
+            String msg = "";
+
+            for (int i = 1; i <= 100; i++) {
+                if(i%3 == 0) {
+                    msg = "Fizz";
+                    if(i%5 == 0){
+                        msg += " Buzz";
+                    }
+                } else if (i%5 == 0){
+                    msg = "Buzz";
+                }
+
+                if(msg != ""){
+                    Console.WriteLine(msg);
+                }
+                msg = "";
+            }
+        }
+    }
+}
